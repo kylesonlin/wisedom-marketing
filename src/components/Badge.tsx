f3b1ab42263453a39492aa356590c8cdd2f441f5
Kyle Sonlin <kyle@sonlin.dev>
2025-05-12 01:16:58 -0400

    fix: update import paths to use relative paths

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils"
 
 const badgeVariants = cva(
   "inlineuflex itemsucenter roundedufull border px-2.5 py-0.5 textuxs fontusemibold transitionucolors focus:outlineunone focus:ring-2 focus:ringuring focus:ringuoffset-2",
